d293470fbc1975d7667e772b4ed2d634b36866ec
Chun <caochun@gmail.com>
2022-02-19 23:44:40 +0800

    update doc

--- tollgate-core/src/main/java/com/example/tollgate/channel/VehicleContextConsumer.java
+++ tollgate-core/src/main/java/com/example/tollgate/channel/VehicleContextConsumer.java
@@ -1,7 +1,6 @@
 package com.example.tollgate.channel;
 
 import com.example.tollgate.model.TollgateService;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.function.Consumer;
