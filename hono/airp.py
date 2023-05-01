4ad88fb472843ca4b17f218e85559397e0bb63b9
Chun <caochun@gmail.com>
2023-05-01 23:11:59 +0800

    update

--- hono/airp.py
+++ hono/airp.py
@@ -1,11 +1,20 @@
-from miio import airpurifier, exceptions
+import schedule
+import time
+
+from miio.integrations.airpurifier.zhimi import airpurifier
+
+purifier = airpurifier.AirPurifier(
+        ip="192.168.80.148", token="acf8b3ced11c488f447b5a662ff3cd95")
+
+def job():
+    print(purifier.get_properties(["aqi"]))
 
 
 def main():
-    airpurifier.AirPurifier(ip="192.168.80.148", token="acf8b3ced11c488f447b5a662ff3cd95")
-    print(airpurifier.status())
-
-
-
+    schedule.every(1).second.do(job)
+    while True:
+        schedule.run_pending()   # 运行所有可以运行的任务
+        time.sleep(1)
+    
 if __name__ == '__main__':
     main()
